b3ee12a2ba024b563d396cd82f935586ec162d79
Yann Bergonzat <yannbergonzat@gmail.com>
2016-03-18 10:58:34 +0100

    Able to connect with nickname

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,7 @@ class ApplicationController < ActionController::Base
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :nickname, :email, :password, :remember_me) }
       devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nickname, :password, :password_confirmation, :current_password, :email, :first_name, :last_name, :address, :zipcode, :city) }
     end
 end
