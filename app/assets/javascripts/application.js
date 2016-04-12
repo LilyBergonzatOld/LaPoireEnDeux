5566b424fbb0e78a8ad3391d46318017031a6926
SolalDR <solal.dussout-revel@hotmail.fr>
2016-04-12 18:00:25 +0200

    add responsive panel filter in farm index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,24 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+function menuFilterResponsive(menuFilter, displayButton) {
+  if (menuFilter.className.match(/after/)) {
+    menuFilter.className = 'col-sm-3 col-xs-12 text-center-xs menu-filter';
+    console.log(menuFilter.className);
+  } else {
+    menuFilter.className += ' after';
+    console.log(menuFilter.className);
+  }
+}
+window.onload = function() {
+  //Si le menu existe alors on est sur une page correspondante
+  if (document.getElementsByClassName('menu-filter')[0]) {
+    var menuFilter = document.getElementsByClassName('menu-filter')[0];
+    var displayButton = document.getElementsByClassName('display-filter')[0];
+    displayButton.addEventListener('click', function() {
+      if (window.innerWidth < 768) {
+        menuFilterResponsive(menuFilter, displayButton);
+      }
+    }, false);
+  }
+}
