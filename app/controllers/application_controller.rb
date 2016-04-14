e5ab3fefa7eedd7065693bde04ceb844dd67c059
SolalDR <solal.dussout-revel@hotmail.fr>
2016-04-14 15:47:25 +0200

    add description field in devise form + add phone field in devise form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :password, :password_confirmation) }
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :nickname, :email, :password, :remember_me) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nickname, :password, :password_confirmation, :current_password, :email, :first_name, :last_name, :address, :zipcode, :city) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:nickname, :password, :password_confirmation, :current_password, :email, :first_name, :last_name, :address, :zipcode, :city, :phone, :description) }
   end
 end
